4a45bebff3be1fff6ecb1a7cd46da1e762641346
ColetonS <coleton.m.smith@gmail.com>
2019-07-26 08:49:05 -0600

    need mentor help

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+require('dotenv').config()
 const express = require("express");
+const massive = require('massive')
 
 const app = express();
 
-const { SERVER_PORT } = process.env;
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+massive(CONNECTION_STRING).then(dbInstance => app.set('db', dbInstance)).catch(err => console.log(err))
+
+// dbInstance.new_planes()
+//   .then(planes => console.log(planes))
+//   .catch(err => console.log(err))
 
 app.use(express.json());
+
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server listening on port ${SERVER_PORT}`);
